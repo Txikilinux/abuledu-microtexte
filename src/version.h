0288555caa7dafa84cd4a5a32cff3c93c51bb6d1
Icham Sirat <icham.sirat@ryxeo.com>
2014-10-09 12:01:30 +0200

    [Separat° projet] #3 Remise en place des spécificités Microtexte

--- src/version.h
+++ src/version.h
@@ -7,14 +7,15 @@
 #define VER_PRODUCTVERSION          1,0,0,0
 #define VER_PRODUCTVERSION_STR      "1.0.0\0"
 
-#define VER_FILEDESCRIPTION_STR     "Tout petit editeur de texte"
-#define VER_INTERNALNAME_STR        "abuledu-minitexte"
-#define VER_ORIGINALFILENAME_STR    "abuledu-minitexte.exe"
-#define VER_PRODUCTNAME_STR         "AbulEdu MiniTexte"
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
+#define VER_FILEDESCRIPTION_STR     "Version maternelle de Minitexte"
+#define VER_INTERNALNAME_STR        "abuledu-microtexte"
 #define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 RyXeo SARL"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    "License GNU/GPL v3"
+#define VER_ORIGINALFILENAME_STR    "abuledu-microtexte.exe"
+#define VER_PRODUCTNAME_STR         "AbulEdu MicroTexte"
+
 #define VER_COMPANYDOMAIN_STR       "abuledu.org"
 
 /** 20130624 : Ajout du numéro de version du module actuellement généré par l'application */
