33a2381e458e46e46de519603bac086a0d46e47a
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2016-01-22 16:42:32 +0100

    Passage en version 1.0.4

--- src/version.h
+++ src/version.h
@@ -1,16 +1,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,2,0
-#define VER_FILEVERSION_STR         "1.0.2\0"
+#define VER_FILEVERSION             1,0,4,0
+#define VER_FILEVERSION_STR         "1.0.4\0"
 
-#define VER_PRODUCTVERSION          1,0,2,0
-#define VER_PRODUCTVERSION_STR      "1.0.2\0"
+#define VER_PRODUCTVERSION          1,0,4,0
+#define VER_PRODUCTVERSION_STR      "1.0.4\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "Version maternelle de Minitexte"
 #define VER_INTERNALNAME_STR        "abuledu-microtexte"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 RyXeo SARL"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2016 RyXeo SARL"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    "License GNU/GPL v3"
 #define VER_ORIGINALFILENAME_STR    "abuledu-microtexte.exe"
